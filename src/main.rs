5c593aae5424b72f26d5b84befe4a7c044b41e49
jumbledFox <81022536+jumbledFox@users.noreply.github.com>
2024-01-18 23:06:46 +0000

    FUUUCK YEEEAAAAH bitches :3
    i kiss boys im a boykisser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::time::Instant;
+use std::{time::Instant, ops::{Div, Mul}};
 
 use winit::{
     event::{Event, WindowEvent},
@@ -85,6 +85,7 @@ fn main() {
     let mut check_points: Vec<Vector2<f64>> = Vec::with_capacity(10);
     let mut hit_pos: Option<Vector2<f64>> = None;
 
+    let mut ray_sweep = -1.0;
     event_loop.run(move |event, control_flow| {
         if let Event::WindowEvent { event, .. } = &event {
             match event {
@@ -100,6 +101,9 @@ fn main() {
         if input.update(&event) {
             deltatime = lasttime.elapsed().as_secs_f64();
             lasttime = Instant::now();
+
+            ray_sweep += deltatime;
+            if ray_sweep > 1.0 { ray_sweep = -1.0; }
 
             // Exiting
             if input.key_pressed(KeyCode::Escape) || input.close_requested() {
@@ -126,43 +130,28 @@ fn main() {
             if input.key_held(KeyCode::KeyS) { mov.y += 1.0; }
             if input.key_held(KeyCode::KeyD) { mov.x += 1.0; }
             if mov.magnitude() != 0.0 { player_pos += mov.normalize() * deltatime * 10.0; }
-            
-            // if input.key_held(KeyCode::KeyW) {  player_pos[1] += deltatime * 10.0 * player_dir[1];
-            //                                     player_pos[0] += deltatime * 10.0 * player_dir[0]; }
-            // if input.key_held(KeyCode::KeyS) {  player_pos[1] -= deltatime * 10.0 * player_dir[1];
-            //                                     player_pos[0] -= deltatime * 10.0 * player_dir[0]; }
-            // if input.key_held(KeyCode::KeyA) {  player_pos[1] += deltatime * 10.0 * player_dir[0];
-            //                                     player_pos[0] -= deltatime * 10.0 * player_dir[1]; }
-            
-            // ?? what is this comment lol
-            // Player diretion = 
-            //player_dir = [(mouse_pos[0]/GRID_SIZE as f64)-player_pos[0], (mouse_pos[1]/GRID_SIZE as f64)-player_pos[1]];
-            player_dir = ((mouse_pos/GRID_SIZE as f64) - player_pos).normalize();
-            // let len = f64::sqrt(player_dir[0].powi(2) + player_dir[1].powi(2));
-            // //player_dir = [player_dir[0] / len / 5.0, player_dir[1] / len / 5.0];
-            // player_dir = [player_dir[0] / len * 0.5, player_dir[1] / len * 0.5];
 
-            //cam_plane = [player_dir[1], -player_dir[0]];
+            // I'm keeping the comment below as a remnant of a simpler time: 
+            // Player diretion = 
+            player_dir = ((mouse_pos/GRID_SIZE as f64) - player_pos).normalize();
             cam_plane = Vector2::new(player_dir.y, -player_dir.x);
 
             struct Ray {
                 pos: Vector2<f64>,
                 dir: Vector2<f64>,
             }
-            let mut ray = Ray { pos: player_pos, dir: player_dir };
-            
+            let mut ray = Ray { pos: player_pos, dir: player_dir + (cam_plane * ray_sweep) };
+            let facing = Vector2::new(ray.dir.x.signum(), ray.dir.y.signum());
             check_points.clear();
             hit_pos = None;
-            
             // TODO: do algorithm, make classes
             for i in 0..100 {
                 // Calculate how far the ray should move and move it.
-                let mov = Vector2::new(1.0, ray.dir.y/ray.dir.x);
-                let mov = Vector2::new(ray.dir.x/ray.dir.y, 1.0);
-                ray.pos += mov;
-                //ray_pos = [ray_pos[0] + mov[0], ray_pos[1] + mov[1]];
-                //ray_pos = [ray_pos[0] + ray_dir[0], ray_pos[1] + ray_dir[1]];
-                //ray_pos = [ray_pos[0] + ray_dir[0], ray_pos[1] + ray_dir[1]];
+                let y_edge = Vector2::new(1.0, ray.dir.y/ray.dir.x) * facing.x; // <- the muliplying by just the x and the y works is
+                let x_edge = Vector2::new(ray.dir.x/ray.dir.y, 1.0) * facing.y; // because the negatives in the fraction !!! 
+                let step = x_edge;
+                let step = ray.dir * 0.5;
+                ray.pos += step;
 
 
                 check_points.push(ray.pos);
@@ -219,17 +208,11 @@ fn draw(screen: &mut [u8], player_pos: &Vector2<f64>, player_dir: &Vector2<f64>,
             x*GRID_SIZE as usize,                      y*GRID_SIZE as usize,
             x*GRID_SIZE as usize + GRID_SIZE as usize, y*GRID_SIZE as usize + GRID_SIZE as usize,
             &col);
-        // This code also works but has lots of floats, my version is better, albeit without bounds checking.. haha
-        // pixels_primitives::square_filled(screen, WIDTH as i32,
-        //     (x as u32*GRID_SIZE)as f64+(GRID_SIZE as f64/2.0),
-        //     (y as u32*GRID_SIZE)as f64+(GRID_SIZE as f64/2.0),
-        //     GRID_SIZE.into(), &[0x00, 0x00, 0xFF, 0xFF]);
     }
-    for i in 0..map.len() {
-        if map[i] == 0 { continue; }
-        let x = i % MAP_WIDTH;
-        let y = i / MAP_WIDTH % MAP_HEIGHT;
-    }
+    // Draw line from player to mouse
+    draw_line(screen,
+        player_pos * GRID_SIZE_F64, *mouse_pos,
+        &[0x77, 0x00, 0x00, 0xFF]);
     // Draw ray
     if let Some(hit_pos) = hit_pos {
         // Draw hit point
@@ -260,9 +243,6 @@ fn draw(screen: &mut [u8], player_pos: &Vector2<f64>, player_dir: &Vector2<f64>,
         (player_pos + player_dir - cam_plane) * GRID_SIZE_F64,
         (player_pos + player_dir + cam_plane) * GRID_SIZE_F64,
         &[0x00, 0xFF, 0xFF, 0xFF]);
-    draw_line(screen,
-        player_pos * GRID_SIZE_F64, *mouse_pos,
-        &[0x77, 0x00, 0x00, 0xFF]);
 }
 
 // A neater way of invoking pixels_primitves functions
